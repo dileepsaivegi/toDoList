a31f3d920385f9e01514352f8ca8df783b91cbd4
dileepsaivegi <91449667+dileepsaivegi@users.noreply.github.com>
2022-12-20 19:06:13 +0530

    Node file

---
+++ app.js
@@ -0,0 +1,170 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const _=require("lodash");
+const url = "mongodb+srv://dileepsaivegi:Y49zRWGZUJVy3p9@cluster0.lr5g4.mongodb.net/toDoListDB?retryWrites=true&w=majority";
+mongoose.set('strictQuery', true);
+app.use(express.urlencoded({
+  extend: true
+}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+mongoose.connect(url);
+mongoose.connection;
+
+
+const Schema = mongoose.Schema;
+
+//Schemas for items and ListNames
+const itemsSchema = new Schema({
+  name: String
+});
+
+const listSchema = new Schema({
+  name: String,
+  listItems: [itemsSchema]
+});
+
+//Model of the items
+const Item = new mongoose.model("item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to the toDoList"
+});
+const item2 = new Item({
+  name: "click + to add a new Item"
+});
+const item3 = new Item({
+  name: "--> Select there to delete Item"
+});
+
+let defaultArray = [item1, item2, item3];
+
+
+
+//Model of the ListNames
+const ListNames = mongoose.model("listName", listSchema);
+//Ignoring the favicon in the database
+app.get('/favicon.ico', (req, res) => res.status(204).end);
+
+
+//Creating the lists based upon their parameter
+app.get("/:workItem", (req, res) => {
+  let urlTitle = _.lodash(req.params.workItem);
+  ListNames.findOne({
+    name: urlTitle
+  }, function(err, foundItems) {
+    if (!err) {
+
+      if (!foundItems) {
+        console.log("doesnt exists");
+        const TitleNames = new ListNames({
+          name: urlTitle,
+          listItems: defaultArray
+        });
+        TitleNames.save();
+        res.redirect("/" + urlTitle);
+      } else {
+        console.log("exists");
+
+        res.render("list", {
+          keyDay: foundItems.name,
+          keyItem: foundItems.listItems
+        });
+      }
+
+    } else {
+      console.log(err);
+    }
+  });
+
+
+});
+
+//Getting the default list items
+app.get("/", function(req, res) {
+  Item.find(function(err, items) {
+    if (items.length === 0) {
+      Item.insertMany(defaultArray, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("default values inserted");
+        }
+      });
+      res.redirect("/");
+    } else {
+      items.forEach(function(items) {
+        console.log();
+      });
+      res.render("list", {
+        keyDay: "today",
+        keyItem: items
+      });
+    }
+  });
+});
+
+
+//Deleting the items from their respective lists
+
+app.post("/delete", function(req, res) {
+  const deleteItem = req.body.checkboxed;
+  const deleteRouteItem = req.body.hiddenValue;
+  if (deleteRouteItem === "today") {
+    Item.findByIdAndRemove(deleteItem, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("success");
+      }
+    });
+    res.redirect("/");
+  } else {
+    ListNames.findOneAndUpdate({
+      name: deleteRouteItem
+    }, {
+      $pull: {
+        listItems: {
+          _id: deleteItem
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
+        res.redirect("/" + deleteRouteItem);
+
+      }
+    });
+  }
+
+});
+
+
+//Inputing the items to thier respective lists using post request
+app.post("/", function(req, res) {
+  const nextItem = req.body.item;
+  const buttonValue = req.body.buttonName;
+  const itemNext = new Item({
+    name: nextItem
+  });
+  if (buttonValue === "today") {
+    itemNext.save();
+    res.redirect("/");
+  } else {
+    ListNames.findOne({
+      name: buttonValue
+    }, function(err, foundList) {
+
+      console.log(foundList.listItems);
+      console.log(foundList.listItems.push(itemNext));
+      console.log(foundList.save());
+      res.redirect("/" + buttonValue);
+    });
+  }
+});
+
+//intiating the server
+app.listen("1000", function(req, res) {
+  console.log("intiated at 1000");
+});
